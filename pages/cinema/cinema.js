279da89f6a9d1360bf417e590ce996755170b5df
sunbey <544712159@qq.com>
2016-12-30 17:03:25 +0800

    Edit home page v2
    Add images

--- pages/cinema/cinema.js
+++ pages/cinema/cinema.js
@@ -1,19 +1,33 @@
-// pages/cinema/cinema.js
 Page({
-  data:{},
-  onLoad:function(options){
-    // 页面初始化 options为页面跳转所带来的参数
+  data: {
+    imgUrls: [
+      'http://img02.tooopen.com/images/20150928/tooopen_sy_143912755726.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175866434296.jpg',
+      'http://img06.tooopen.com/images/20160818/tooopen_sy_175833047715.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000
   },
-  onReady:function(){
-    // 页面渲染完成
+  changeIndicatorDots: function(e) {
+    this.setData({
+      indicatorDots: !this.data.indicatorDots
+    })
   },
-  onShow:function(){
-    // 页面显示
+  changeAutoplay: function(e) {
+    this.setData({
+      autoplay: !this.data.autoplay
+    })
   },
-  onHide:function(){
-    // 页面隐藏
+  intervalChange: function(e) {
+    this.setData({
+      interval: e.detail.value
+    })
   },
-  onUnload:function(){
-    // 页面关闭
+  durationChange: function(e) {
+    this.setData({
+      duration: e.detail.value
+    })
   }
 })
