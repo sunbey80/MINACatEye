2fcdf9b1d70beff9df3202288479fff51c47229b
sunbey80 <544712159@qq.com>
2017-01-01 13:39:29 +0800

    Get API
    Get api from NodeCatEye

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,7 +1,33 @@
-// pages/home/home.js
+var app = getApp()
+
 Page({
-  data:{},
-  onLoad:function(options){
-    
-  }
+    data:{
+        G_DOMAIN: app.globalData.G_DOMAIN,
+        onsale: 'true'
+    },
+    onLoad:function(){
+
+        this.findMovies({onsale:this.data.onsale})
+        
+    },
+    switchOnSale: function(e){
+        this.setData({
+            onsale: e.target.dataset.onsale
+        })
+        this.findMovies({onsale:this.data.onsale})
+    },
+    findMovies: function(queryData){
+        var that = this
+        wx.request({
+            url: 'http://'+ app.globalData.G_DOMAIN +'/APImovies',
+            data: queryData,
+            method: 'GET', 
+            success: function(res){
+                // success
+                that.setData({
+                    movies:res.data
+                })
+            }
+        })
+    }
 })
